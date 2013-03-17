b569a43dcef75dd74cba7a0e7b2f65dc87bbbb73
Manik Rathee <manikrathee@gmail.com>
2013-03-17 13:57:14 -0700

    added grid

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+ * scripts.js
+ *
+ * auto-concatenated jquery, modernizr and custom alternate files
+ *
+ */
+
+//@codekit-prepend "libs/jquery-1.7.1.min.js";
+//@codekit-prepend "libs/ss-standard.js";
+//@codekit-prepend "libs/ss-social.js";
+
