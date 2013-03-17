01b428456bc624fb0dbc86dadcfc5528df100059
Manik Rathee <manikrathee@gmail.com>
2013-03-17 16:25:58 -0700

    added back to top logic

--- js/script-ck.js
+++ js/script-ck.js
@@ -110,7 +110,7 @@ var doc = $(document);
 // Add .scroll class to body if window scrolled past 10
 
 win.scroll(function() {
-    if ($(window).scrollTop() > 5) {
+    if ($(window).scrollTop() > 8) {
         $('#intro').addClass('scroll');
     }
 });
@@ -119,4 +119,13 @@ doc.scroll(function() {
     if ($(window).scrollTop()) {
         $('#intro').removeClass('scroll');
     }
+});
+
+
+
+var backToTop = $('#back-to-top');
+
+backToTop.click(function(e){
+    e.preventDefault();
+    $('body').animate({ scrollTop: '0' });
 });
